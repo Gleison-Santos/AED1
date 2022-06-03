fffdc0d648b0bd33a357dc382caa3ca87b7c579e
Gleison-Santos <104906410+Gleison-Santos@users.noreply.github.com>
2022-06-03 11:42:10 -0300

    Delete lab 4/TAD PONTO directory

--- lab 4/TAD PONTO/ponto.h
+++
@@ -1,7 +0,0 @@
-typedef struct ponto Ponto;
-
-Ponto * gera_pto();
-int set_pto(Ponto *p, int a, int b);
-int get_pto(Ponto *p, int *a, int *b);
-float Distancia_pto(Ponto *p1, Ponto *p2);
-void libera_pto(Ponto **p);
